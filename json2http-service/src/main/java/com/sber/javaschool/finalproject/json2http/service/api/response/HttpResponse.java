74baef2f54d675cb2152ad5047b725c42af0fa73
Andrew <Andrey.Zaporozhtsev@yandex.ru>
2021-11-24 23:15:50 +0300

    Json2http: add pretty print for HttpResponse

--- json2http-service/src/main/java/com/sber/javaschool/finalproject/json2http/service/api/response/HttpResponse.java
+++ json2http-service/src/main/java/com/sber/javaschool/finalproject/json2http/service/api/response/HttpResponse.java
@@ -2,6 +2,8 @@ package com.sber.javaschool.finalproject.json2http.service.api.response;
 
 import com.sber.javaschool.finalproject.json2http.service.http.HttpHeaders;
 
+import java.text.MessageFormat;
+
 public interface HttpResponse {
     int statusCode();
 
@@ -10,4 +12,12 @@ public interface HttpResponse {
     String body();
 
     String contentType();
+
+    default String toPrettyPrintString() {
+        return MessageFormat.format("status={0}," +
+                        " headers={1}, " +
+                        " contentType={2}," +
+                        " body={3}",
+                headers(), statusCode(), contentType(), body());
+    }
 }
